c4d917ec2afe0e8b3fcf5d38c11e63a8fed0f853
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-29 15:00:40 -0300

    Set kitten validations & Flash messages

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,9 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :age, presence: true,
+                  numericality: { greater_than: 0, less_than: 50}
+  validates :cuteness, presence: true,
+                       numericality: { greater_than: 6, less_than: 10}
+  validates :softness, presence: true,
+                       numericality: { greater_than: 6, less_than: 10}
 end
